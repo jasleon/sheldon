f8efbb97d1523c11c9f0cede0a79f0c1cb66a9a1
Antonio Sanchez <antonio.sanchez.leon@outlook.com>
2021-10-22 07:39:30 -0500

    Pass arguments to the helloEtl main

--- project/src/examples/etl.cpp
+++ project/src/examples/etl.cpp
@@ -1,7 +1,12 @@
 #include "etl.hpp"
 
-int main() {
-  ETL etl("data.csv", true);
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cout << "usage: helloEtl path-to-csv\n";
+    return -1;
+  }
+  std::string path(argv[1]);
+  ETL etl(path, true);
   etl.load();
   etl.show();
   auto matrix = etl.to_matrix();
