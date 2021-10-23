8ef0406aa1c49ef052030c1d8513787049b5c800
Antonio Sanchez <antonio.sanchez.leon@outlook.com>
2021-10-23 10:56:38 -0500

    Use a scatter plot to visualize the data

--- project/src/examples/etl.cpp
+++ project/src/examples/etl.cpp
@@ -1,5 +1,7 @@
 #include "etl.hpp"
 
+#include <sciplot/sciplot.hpp>
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     std::cout << "usage: helloEtl path-to-csv\n";
@@ -8,9 +10,30 @@ int main(int argc, char **argv) {
   std::string path(argv[1]);
   ETL etl(path, true);
   etl.load();
-  etl.show();
+
+  // Convert CSV to Matrix
   auto matrix = etl.to_matrix();
-  std::cout << "Here's in matrix form:\n";
-  std::cout << matrix << std::endl;
+  auto x = matrix.col(0);
+  auto y = matrix.col(1);
+
+  // Create a Plot object
+  sciplot::Plot plot;
+
+  // Set color palette
+  plot.palette("set2");
+
+  // Set the legend
+  plot.legend().hide();
+
+  // Set the x and y labels
+  plot.xlabel("Population of City in 10,000s");
+  plot.ylabel("Profit in $10,000s");
+
+  // Draw a scatter plot to visualize the data
+  plot.drawPoints(x, y).pointType(2);
+
+  // Show the plot in a pop-up window
+  plot.show();
+
   return 0;
 }
